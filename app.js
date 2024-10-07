6ad424924a93c14ed6c15e8b4278def18bcc038b
Ulugbek Mannonov <ulugbek.sbk.2021@gmail.com>
2024-10-07 17:13:20 +0800

    feat: routerni qurib olish

--- app.js
+++ app.js
@@ -1,21 +1,18 @@
 console.log("Web Serverni Boshlash");
 const express = require("express");
 const app = express();
-
+const router = require("./router");
 
 // MONGODB chaqirish
 const db = require("./server").db();
 const mongodb = require("mongodb");
-
 
 // 1. Kirish codelari
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
 // 2. Session codes
-
 
 // 3. Views code
 app.set("views", "views");
@@ -23,4 +20,7 @@ app.set("view engine", "ejs");
 
 
 // 4. Routing code
+app.use("/", router);
+
+
 module.exports = app;
