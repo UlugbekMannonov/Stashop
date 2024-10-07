b129119989a55520f56e51d65d9a08dfc9beae57
Ulugbek Mannonov <ulugbek.sbk.2021@gmail.com>
2024-10-07 17:46:45 +0800

    feat: building member controllers

--- router.js
+++ router.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const router = express.Router();
+const memberController = require("./controllers/memberController");
 
-router.get("/", function (req, res) {
-  res.send("Home sahifasidasiz");
-});
+// Memberga dahildor routerlar
+router.get("/", memberController.home);
+router.post("/signup", memberController.signup);
+router.post("/login", memberController.login);
+router.get("/logout", memberController.logout);
+
+// Boshqa routerlar
 
 router.get("/menu", (req, res) => {
   res.send("Menu sahifasidasiz");
